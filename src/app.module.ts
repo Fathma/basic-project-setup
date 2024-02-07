0458a1095bf75fd7a061632faa4f758cf5e93809
fathma <siddiquefathma@gmail.com>
2024-02-07 15:04:13 +0600

    testing pre commit 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './users/users.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UserModule } from './users/users.module'
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common'
 
 @Controller('users')
 export class UsersController {
-    @Get()
-    getUsers() {
-        return "Users"
-    }
+  @Get()
+  getUsers (): string {
+    return 'Users'
+  }
 
-    @Get('/:id')
-    getUser(@Param('id') id: string) {
-        return `User ${id}`
-    }
+  @Get('/:id')
+  getUser (@Param('id') id: string): string {
+    return `User ${id}`
+  }
 }
